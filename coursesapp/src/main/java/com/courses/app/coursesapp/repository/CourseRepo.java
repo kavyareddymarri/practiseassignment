2843302267b69e8c8d58e715d42cff6763e1c790
kavya <kavya123.marri@gmail.com>
2023-11-02 17:39:10 +0100

    Docker updated

--- coursesapp/src/main/java/com/courses/app/coursesapp/repository/CourseRepo.java
+++ coursesapp/src/main/java/com/courses/app/coursesapp/repository/CourseRepo.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface CourseRepo extends JpaRepository<CourseEntity,Long>{
 
   // @Query(nativeQuery = true,value = "select cs.courseid, cs.course_name,cs.startdate,cs.enddate,cs.price,cs.registered from courses.course_entity cs join customers.student_entity std on std.id=cs.student_id where cs.student_id=:studentid")
-   @Query(nativeQuery = true,value = "select * from courses.course_entity cs join customers.student_entity std on std.id=cs.student_id where cs.student_id=:studentid")
+   @Query(nativeQuery = true,value = "select * from courses.course_entity cs join students.student_entity std on std.id=cs.student_id where cs.student_id=:studentid")
    List<CourseEntity> findbystudentid(@Param("studentid") Long studentid);
 
 
